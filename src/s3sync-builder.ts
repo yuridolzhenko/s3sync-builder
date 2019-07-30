f7e81c778ed8436d6336e5ae51c94d69fa731ee9
Yuri Dolzhenko <yuri.dolzhenko@gmail.com>
2019-07-30 22:40:33 +0200

    almost there.

--- src/s3sync-builder.ts
+++ src/s3sync-builder.ts
@@ -1,8 +1,10 @@
 import {BuilderOutput, createBuilder} from '@angular-devkit/architect';
 import {JsonObject} from '@angular-devkit/core';
 import * as AWS from 'aws-sdk';
-import {ConfigurationOptions} from "aws-sdk/lib/config";
-import {DeleteObjectRequest, ListObjectsV2Output, ListObjectsV2Request, Object, ObjectKey} from "aws-sdk/clients/s3";
+import {DeleteObjectRequest, ListObjectsV2Output, ListObjectsV2Request, Object} from "aws-sdk/clients/s3";
+import {from, Observable, of} from "rxjs";
+import {count, mergeAll, mergeMap, tap} from "rxjs/operators";
+import * as glob from "glob";
 
 interface Options extends JsonObject {
 
@@ -45,22 +47,6 @@ export default createBuilder<Options>((options, context) => {
             Bucket: options.targetBucket
         };
 
-        let deleteObjects = function deleteContents(bucketContents: Object[]) {
-            return new Promise((resolve, reject) => {
-                console.info(bucketContents);
-                for (let entry in bucketContents) {
-                    let key: string = bucketContents[entry].Key as string;
-
-                    let deleteObjectRequest : DeleteObjectRequest = {
-                      Bucket: options.targetBucket,
-                      Key: key
-                    };
-                    S3.deleteObject(deleteObjectRequest).promise();
-                    console.info(key);
-                }
-            })
-        };
-
         let scanBucketContents = function scanBucketContents(request: ListObjectsV2Request): Promise<Object[]> {
             const intermediateRequest: ListObjectsV2Request = Object.assign({}, request);
             return new Promise(resolve => {
@@ -71,8 +57,10 @@ export default createBuilder<Options>((options, context) => {
                             if (response.Contents !== undefined) {
                                 resolve(response.Contents.concat(array));
                             } else {
-                                reject("");
+                                resolve(array);
                             }
+                        }, err => {
+                            reject(err);
                         });
                     } else {
                         resolve(response.Contents);
@@ -83,6 +71,18 @@ export default createBuilder<Options>((options, context) => {
 
             });
         };
+
+        // let scanFolder = function getAllFiles(dirPath: string): void {
+        //     fs.readdirSync(dirPath).forEach(function(file) {
+        //         let filepath = path.join(dirPath , file);
+        //         let stat= fs.statSync(filepath);
+        //         if (stat.isDirectory()) {
+        //             getAllFiles(filepath);
+        //         } else {
+        //             console.info(filepath+ '\n');
+        //         }
+        //     });
+        // };
 
         let scanLocalContents = function scanLocalContents(localContentsPath: string): Promise<Object[]> {
             return Promise.resolve([]);
@@ -96,15 +96,50 @@ export default createBuilder<Options>((options, context) => {
             return Promise.resolve(true);
         };
 
-        const path: string = '/Users/ydolzhenko/Projects/deploy-tester-app/src';
+        const buildPath: string = '/Users/ydolzhenko/Projects/deploy-tester-app/src';
 
-        scanBucketContents(listRequest)
-            .then((objects: Object[]) => {
-                return deleteObjects(objects);
-            }).then(() => {
-                console.info('here');
-            });
 
+        const syncObservable: Observable<any> = from(scanBucketContents(listRequest))
+            .pipe(
+                mergeAll(),
+                tap((bucketEntry) => {
+                    console.info('processing ' + bucketEntry.Key);
+                }),
+                mergeMap((bucketEntry) => {
+                    let bucketEntryKey: string = bucketEntry.Key as string;
+                    let deleteObjectRequest: DeleteObjectRequest = {
+                        Bucket: options.targetBucket,
+                        Key: bucketEntryKey
+                    };
+                    return from(S3.deleteObject(deleteObjectRequest).promise());
+                }),
+                count(),
+                tap((totalCount: number) => {
+                    console.info('deleted ' + totalCount);
+                }),
+                mergeMap(whoCares => {
+                    return of(glob.sync(`/**`, {
+                        root: buildPath,
+                        nodir: true,
+                        matchBase: true
+                    }));
+                }),
+                mergeAll(),
+                tap((newBucketEntry) => {
+                    console.info(newBucketEntry);
+                })
+            );
+
+
+        syncObservable.subscribe(whoCares => {
+        }, error => {
+            console.info('got error');
+            console.info(error);
+            resolve({success: false});
+        }, () => {
+            console.info('completed');
+            resolve({success: true});
+        });
 
         //
         context.reportStatus(`Executing "${options.command}"...`);
